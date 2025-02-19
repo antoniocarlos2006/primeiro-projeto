c481b815fab8a7b61e8efd5d30572875ad619c01
antoniocarlos2006 <antonio.carlos2006jr@gmail.com>
2025-02-18 22:23:58 -0300

    atualizando

--- backend/models/Associacao.js
+++
@@ -1,9 +0,0 @@
-class Associacao {
-    constructor(id, produtoId, fornecedorId) {
-      this.id = id;
-      this.produtoId = produtoId;
-      this.fornecedorId = fornecedorId;
-    }
-  }
-  
-  module.exports = Associacao;
